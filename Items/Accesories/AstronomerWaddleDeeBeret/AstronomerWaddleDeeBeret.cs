3099c82bfd39f34cae7ab7d3b8095c73a147aac3
Photonic0 <soncelladaniel@gmail.com>
2025-09-13 02:58:12 -0300

    astronomer waddle dee beret

---
+++ Items/Accesories/AstronomerWaddleDeeBeret/AstronomerWaddleDeeBeret.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace KirboMod.Items.Accesories.AstronomerWaddleDeeBeret
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class AstronomerWaddleDeeBeret : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
+        }
+        public override void SetDefaults()
+        {
+            Item.vanity = true;
+            Item.accessory = true;
+            Item.width = 30;
+            Item.height = 14;
+        }
+
+    }
+}
